8e9d8aa7c1abe0febbe8f449c70700fc87577d9c
ugwuejeh <ugwudavidejeh@gmail.com>
2023-04-15 04:13:26 +0100

    Update listq2.py

--- listq2.py
+++ listq2.py
@@ -1,14 +1,15 @@
-# a program that generates a list of 15 random integers, finds the largest and smallest numbers in the list, and prints them out:
+
+#  Python program that sums up all the items in a list of 9 items, removes duplicates, and prints the results
 
 
-import random
-# we used random sample to Generate a list of 15 random integers between 0 and 100, with 15 as tolal numbere of list.
-my_list = random.sample(range(0, 100), 15) 
-print("List of numbers:", my_list)
-# max() is an inbuild function for Finding  the largest number in the list
-largest_num = max(my_list) 
-print("Largest number in the list:", largest_num)
-# min() is an inbuild function for Finding the smallest number in the list
-smallest_num = min(my_list) 
-print("Smallest number in the list:", smallest_num)
+my_list = [1, 2, 3, 5, 5, 6, 6, 8, 9]
 
+# we remove duplicates with the help of set function.
+my_list2 = list(set(my_list))
+print("this is my new list after duplicate has been removed",my_list2)
+
+# Sum up all the items in the list
+total_sum = sum(my_list2)
+
+# Print the result
+print("The sum of all the items in the list is:", total_sum)
